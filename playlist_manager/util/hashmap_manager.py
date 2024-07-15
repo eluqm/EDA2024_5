b8ee7856a1a148100427e881cdc4e6478357c2f7
FernandoGarambelM <fgarambel@unsa.edu.pe>
2024-07-14 23:13:04 -0500

    feat: Plantilla basica para hashmap

--- playlist_manager/util/hashmap_manager.py
+++ playlist_manager/util/hashmap_manager.py
@@ -1,1 +1,26 @@
 # playlist_manager/util/hashmap_manager.py
+class HashMap:
+    def __init__(self, size):
+        self.size = size
+        self.table = [[] for _ in range(size)]
+
+    def _hash(self, key):
+        return hash(key) % self.size
+
+    def insert(self, key, value):
+        hash_key = self._hash(key)
+        bucket = self.table[hash_key]
+        for i, kv in enumerate(bucket):
+            k, v = kv
+            if key == k:
+                bucket[i] = (key, value)
+                return
+        bucket.append((key, value))
+
+    def get(self, key):
+        hash_key = self._hash(key)
+        bucket = self.table[hash_key]
+        for k, v in bucket:
+            if key == k:
+                return v
+        return None
