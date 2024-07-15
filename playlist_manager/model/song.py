8ab35bbb6953ced6dda34ca44f71bbbd86686a89
FernandoGarambelM <fgarambel@unsa.edu.pe>
2024-07-14 23:12:15 -0500

    feat: Se agrega la clase song

--- playlist_manager/model/song.py
+++ playlist_manager/model/song.py
@@ -1,1 +1,25 @@
-# playlist_manager/model/song.py
+class Song:
+    def __init__(self, song_id, artist_name, track_name, track_id, popularity, year, genre, danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, tempo, duration_ms, time_signature):
+        self.song_id = song_id
+        self.artist_name = artist_name
+        self.track_name = track_name
+        self.track_id = track_id
+        self.popularity = popularity
+        self.year = year
+        self.genre = genre
+        self.danceability = danceability
+        self.energy = energy
+        self.key = key
+        self.loudness = loudness
+        self.mode = mode
+        self.speechiness = speechiness
+        self.acousticness = acousticness
+        self.instrumentalness = instrumentalness
+        self.liveness = liveness
+        self.valence = valence
+        self.tempo = tempo
+        self.duration_ms = duration_ms
+        self.time_signature = time_signature
+
+    def __repr__(self):
+        return f"Song({self.song_id}, {self.artist_name}, {self.track_name}, {self.track_id}, {self.popularity}, {self.year}, {self.genre})"
