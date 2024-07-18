e2957435db18887298b50291ce536bcfa9e38d58
FernandoGarambelM <fgarambel@unsa.edu.pe>
2024-07-17 23:27:36 -0500

    feat: Se agrega eliminar a Hashmap

--- playlist_manager/util/hashmap_manager.py
+++ playlist_manager/util/hashmap_manager.py
@@ -1,13 +1,27 @@
 # playlist_manager/util/hashmap_manager.py
+
 class HashMap:
-    def __init__(self, size):
+    def __init__(self, size=1000):
         self.size = size
+        self.count = 0
         self.table = [[] for _ in range(size)]
 
     def _hash(self, key):
         return hash(key) % self.size
 
+    def _resize(self):
+        new_size = self.size * 2
+        new_table = [[] for _ in range(new_size)]
+        for bucket in self.table:
+            for k, v in bucket:
+                new_hash = hash(k) % new_size
+                new_table[new_hash].append((k, v))
+        self.size = new_size
+        self.table = new_table
+
     def insert(self, key, value):
+        if self.count / self.size > 0.7:
+            self._resize()
         hash_key = self._hash(key)
         bucket = self.table[hash_key]
         for i, kv in enumerate(bucket):
@@ -16,6 +30,7 @@ class HashMap:
                 bucket[i] = (key, value)
                 return
         bucket.append((key, value))
+        self.count += 1
 
     def get(self, key):
         hash_key = self._hash(key)
@@ -24,3 +39,20 @@ class HashMap:
             if key == k:
                 return v
         return None
+
+    def delete(self, key):
+        hash_key = self._hash(key)
+        bucket = self.table[hash_key]
+        for i, kv in enumerate(bucket):
+            k, v = kv
+            if key == k:
+                del bucket[i]
+                self.count -= 1
+                return
+
+    def get_all_keys(self):
+        keys = []
+        for bucket in self.table:
+            for k, v in bucket:
+                keys.append(k)
+        return keys
