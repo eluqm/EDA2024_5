1bf18eca4266c6abb563614b12d1e5d89f2b02cb
FernandoGarambelM <fgarambel@unsa.edu.pe>
2024-07-17 22:31:48 -0500

    feat: Nueva estructura para main.py

--- playlist_manager/main.py
+++ playlist_manager/main.py
@@ -1,22 +1,58 @@
 # playlist_manager/main.py
+
 from util.file_manager import FileManager
-from EDA2024_5.playlist_manager.util.bplustree_manager import BTree
+from util.bplustree_manager import BPlusTree
 from util.hashmap_manager import HashMap
 
 file_path = 'data/spotify_data.csv'
 file_manager = FileManager(file_path)
-songs = file_manager.load_songs()
+file_manager.load_songs()
 
-# Crear B-Tree y HashMap
-btree = BTree(t=3)
-hashmap = HashMap(size=10000)
+trie = file_manager.trie
+hashmap = HashMap()
+#bplustree = BPlusTree(3)
 
-# Insertar canciones en B-Tree y HashMap
-for song in songs:
-  #   btree.insert(song.popularity, song)
-    hashmap.insert(song.song_id, song)
+# Funciones
+def buscar_y_agregar_cancion():
+    while True:
+        song_name = input("Ingrese el nombre de la canción que desea agregar (o 'terminar' para finalizar): ").strip()
+        if song_name.lower() == 'terminar':
+            break
+        song_ids = trie.search(song_name)
+        if song_ids:
+            for song_id in song_ids:
+                song = file_manager.get_song_by_id(song_id)
+                if song:
+                    hashmap.insert(song_id, song)
+                    print(f"Se agregó la canción: {song.track_name}")
+                else:
+                    print(f"No se encontró la canción con ID {song_id}")
+        else:
+            print("No se encontraron canciones con ese nombre.")
 
-# Ejemplo de búsqueda
-#song_by_popularity = btree.search(85)  # Buscar canción por popularidad
-song_by_id = hashmap.get('2')  # Buscar canción por ID
-print(song_by_id)
+def mostrar_playlist():
+  pass
+
+def ordenar_playlist():
+  pass
+
+# Interacción con el usuario
+while True:
+    print("\nOpciones:")
+    print("1. Buscar y agregar canciones")
+    print("2. Mostrar lista de reproducción")
+    print("3. Ordenar lista de reproducción")
+    print("4. Salir")
+    opcion = input("Seleccione una opción: ").strip()
+
+    if opcion == '1':
+        buscar_y_agregar_cancion()
+    elif opcion == '2':
+        mostrar_playlist()
+    elif opcion == '3':
+        ordenar_playlist()
+    elif opcion == '4':
+        print("Saliendo...")
+        break
+    else:
+        print("Opción inválida. Intente de nuevo.")
