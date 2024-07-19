b6ce25c3c8c6f71fd751d1f6d2d9b6120ef3664f
Choflis <lluquecon@unsa.edu.pe>
2024-07-19 00:58:42 -0500

    mejoramos la implementación de b++

--- playlist_manager/util/bplustree_manager.py
+++ playlist_manager/util/bplustree_manager.py
@@ -1,12 +1,11 @@
 # playlist_manager/util/bplustree_manager.py
-
 class BPlusTreeNode:
     def __init__(self, t, leaf=False):
-        self.t = t  # Grado mínimo (t) del B++ Tree
-        self.leaf = leaf  # Verifica si es un nodo hoja
-        self.keys = []  # Lista de claves
-        self.children = []  # Lista de hijos
-        self.next = None  # Apunta al siguiente nodo hoja
+        self.t = t
+        self.leaf = leaf
+        self.keys = []
+        self.children = []
+        self.next = None
 
 class BPlusTree:
     def __init__(self, t):
@@ -23,23 +22,22 @@ class BPlusTree:
             self._insert_non_full(temp, key, value)
         else:
             self._insert_non_full(root, key, value)
-    
+
     def _split_child(self, parent, index):
         t = self.t
         node = parent.children[index]
         new_node = BPlusTreeNode(t, node.leaf)
         parent.children.insert(index + 1, new_node)
-        parent.keys.insert(index, node.keys[t-1])
-        new_node.keys = node.keys[t:(2*t-1)]
-        node.keys = node.keys[0:(t-1)]
+        parent.keys.insert(index, node.keys[t-1][0])
+        new_node.keys = node.keys[t:]
+        node.keys = node.keys[:t-1]
 
         if not node.leaf:
-            new_node.children = node.children[t:(2*t)]
-            node.children = node.children[0:(t-1)]
+            new_node.children = node.children[t:]
+            node.children = node.children[:t]
         else:
             new_node.next = node.next
             node.next = new_node
-    
 
     def _insert_non_full(self, node, key, value):
         if node.leaf:
@@ -62,7 +60,7 @@ class BPlusTree:
 
     def search(self, key):
         return self._search(self.root, key)
-        
+
     def _search(self, node, key):
         index = 0
         while index < len(node.keys) and key > node.keys[index][0]:
@@ -73,3 +71,10 @@ class BPlusTree:
             return None
         return self._search(node.children[index], key)
 
+    def display(self, node=None, level=0):
+        if node is None:
+            node = self.root
+        print('Level', level, 'Keys:', node.keys)
+        if not node.leaf:
+            for child in node.children:
+                self.display(child, level + 1)
