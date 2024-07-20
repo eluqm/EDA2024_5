00fd91cc8441c72a8370fafb35c82edefa0dbbfc
Choflis <lluquecon@unsa.edu.pe>
2024-07-20 17:36:47 -0500

    el ordenamiento no funciona porque falta poner los atributos

--- playlist_manager/main.py
+++ playlist_manager/main.py
@@ -1,11 +1,20 @@
 import tkinter as tk
-from tkinter import ttk, messagebox, simpledialog
 from util.playlist_manager import PlaylistManager
 from util.gui_manager import GUIManager
 
-if __name__ == "__main__":
+def main():
     root = tk.Tk()
-    file_path = 'data/spotify_data.csv'
-    playlist_manager = PlaylistManager(3, file_path)
+    file_path = 'data/spotify_data.csv'  # Asegúrate de que esta ruta sea correcta
+    bplustree_order = 3  # Ajusta el orden del B+ tree según sea necesario
+
+    # Crear instancia de PlaylistManager con el orden del B+ tree y la ruta del archivo CSV
+    playlist_manager = PlaylistManager(bplustree_order, file_path)
+
+    # Crear instancia de GUIManager con la ventana principal y el PlaylistManager
     gui_manager = GUIManager(root, playlist_manager)
+
+    # Iniciar el bucle principal de Tkinter
     root.mainloop()
+
+if __name__ == "__main__":
+    main()
