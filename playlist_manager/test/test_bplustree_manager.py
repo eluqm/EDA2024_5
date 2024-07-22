bfcf8c2714cddb5d5d8e491585af26c53e17be6f
RaqDxs <aquispearr@unsa.edu.pe>
2024-07-21 20:55:47 -0500

    Prueba de insercion y busqueda de los nodos

--- playlist_manager/test/test_bplustree_manager.py
+++ playlist_manager/test/test_bplustree_manager.py
@@ -3,7 +3,17 @@ from util.bplustree_manager import BPlusTree, BPlusTreeNode
 
 class TestBPlusTree(unittest.TestCase):
     def setUp(self):
-        self.tree = BPlusTree(3)  # Crear un árbol B+ con orden 3
+        self.tree = BPlusTree(3)  # c crea un árbol B+ con orden 3
 
 if __name__ == '__main__':
     unittest.main()
+
+def test_insert_and_search(self):
+    self.tree.insert(10, "value10")
+    self.tree.insert(20, "value20")
+    self.tree.insert(5, "value5")
+
+    self.assertEqual(self.tree.search(10), "value10")
+    self.assertEqual(self.tree.search(20), "value20")
+    self.assertEqual(self.tree.search(5), "value5")
+    self.assertIsNone(self.tree.search(15))
